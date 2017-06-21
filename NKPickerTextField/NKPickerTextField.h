c5a5cee63408dc8443616cca2233555654660b59
Nikolai Kratz <nikolai.kratz@gmail.com>
2017-06-21 11:30:31 +0200

    Initial Commit

---
+++ NKPickerTextField/NKPickerTextField.h
@@ -0,0 +1,19 @@
+//
+//  NKPickerTextField.h
+//  NKPickerTextField
+//
+//  Created by Nikolai Kratz on 21.06.17.
+//  Copyright © 2017 Nikolai Kratz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NKPickerTextField.
+FOUNDATION_EXPORT double NKPickerTextFieldVersionNumber;
+
+//! Project version string for NKPickerTextField.
+FOUNDATION_EXPORT const unsigned char NKPickerTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NKPickerTextField/PublicHeader.h>
+
+
